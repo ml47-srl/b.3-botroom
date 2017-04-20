2b50087b7f7ec7f062b9175a7cb2ddd713e18596
memoryleak47 <memoryleak47@protonmail.ch>
2017-04-20 08:05:16 +0200

    version system now only uses the newest version of a bot

--- botroom.sh
+++ botroom.sh
@@ -5,65 +5,37 @@ die() {
 	exit 1
 }
 
-[[ ! -d botpairs ]] && mkdir botpairs
-
-# eg. botpair = "linbot-23d4c"
-
-init_botcache() {
-	[[ ! -d botcache ]] && mkdir botcache
+init_bots() {
+	[[ ! -d bots ]] && mkdir bots
 	for bot in $(get_bots)
 	do
-		if [ -d "botcache/$bot" ]; then
-			(cd "botcache/$bot"; git pull origin master)
+		local bot_path="bots/$bot"
+		local reload_bot="false"
+
+		if [ -d "$bot_path" ]; then
+			(cd "$bot_path/code"
+				x="$(git rev-list master)"
+				git pull origin master
+				if [ ! "$x" == "$(git rev-list master)" ]; then
+					reload_bot="true"
+					rm -rf ../i* ../bin
+				fi
+			)
 		else
-			git clone "https://github.com/ml47-srl/b.3-$bot.git" botcache/$bot
+			git clone "https://github.com/ml47-srl/b.3-$bot.git" bots/$bot/code
+			reload_bot="true"
+		fi
+
+		if [ "$reload_bot" == "true" ]; then
+			cp -r botwrapper $bot_path # created bots/linbot/botwrapper
+			(cd $bot_path/botwrapper; cargo build)
+			mv $bot_path/botwrapper/target/debug/botwrapper $bot_path/bin
+			rm -rf $bot_path/botwrapper
 		fi
 	done
 }
 
-# $1 = bot
-get_revs() {
-	(cd "botcache/$1"; git rev-list master)
-}
-
-create_botpair() {
-	botpair="$1"
-	bot=$(bot_from_botpair $botpair)
-	rev=$(rev_from_botpair $botpair)
-	local botpair_path="botpairs/$botpair"
-	mkdir $botpair_path
-	cp -r botwrapper $botpair_path # created botpairs/linbot-23d4c/botwrapper
-	cp -r "botcache/$bot" $botpair_path/bot # created botpairs/linbot-23d4c/bot
-	(cd $botpair_path/bot; git checkout -q "$rev")
-	(cd $botpair_path/botwrapper; cargo build)
-	mv $botpair_path/botwrapper/target/debug/botwrapper $botpair_path/bin
-	(cd $botpair_path; ls | grep -v ^bin$ | xargs rm -rf)
-}
-
-create_missing_botpairs() {
-	for bot in $(get_bots)
-	do
-		for rev in $(get_revs $bot)
-		do
-			local botpair="$bot-$rev"
-			local botpair_path="botpairs/$botpair"
-			[[ -f $botpair_path ]] && die 'Here is a file? -- snh'
-			if [ ! -d $botpair_path ]; then
-				create_botpair $botpair
-			fi
-		done
-	done
-}
-
-bot_from_botpair() {
-	echo ${1%-*}
-}
-
-rev_from_botpair() {
-	echo ${1#*-}
-}
-
-# @botpair
+# @bot
 # $1 = instance
 instance_work() {
 	(cd i$1
@@ -71,15 +43,15 @@ instance_work() {
 	)
 }
 
-# @botpair
+# @bot
 count_instances() {
 	ls | grep ^i[0-9]*$ | wc -l
 }
 
-# $1 = botpair
-exec_botpair() {
-	echo "Executing botpair: '$1'"
-	(cd botpairs/$1
+# $1 = bot
+exec_bot() {
+	echo "Executing bot: '$1'"
+	(cd bots/$1
 		local count=$(count_instances)
 		local newest=$(($count - 1))
 		local instance=$newest
@@ -104,44 +76,37 @@ exec_botpair() {
 }
 
 get_bots() {
-	cat bots
+	cat botnames
 }
 
-get_botpairs() {
-	(cd botpairs
-		ls
-	)
-}
-
-get_botpair_with_highest_prio() {
-	local botpairs=$(get_botpairs)
-	local max_botpair=${botpairs[0]}
-	for botpair in $botpairs
+get_bot_with_highest_prio() {
+	local bots=$(get_bots)
+	local max_bot=${bot[0]}
+	for bot in $bots
 	do
-		if [ $(get_prio $botpair) -gt $(get_prio $max_botpair) ]; then
-			max_botpair=$botpair
+		if [ $(get_prio $bot) -gt $(get_prio $max_bot) ]; then
+			max_bot=$bot
 		fi
 	done
-	echo $max_botpair
+	echo $max_bot
 }
 
-# $1 = botpair
-get_prio() { # 1/(1+number_of_execs) * 2^niceness * 1/(1+number_of_commits_behind)
+# $1 = bot
+get_prio() { # 1/(1+number_of_execs) * 2^niceness?
 	echo '1' # TODO
 	# $(()) is only int!
 }
 
-exec_correct_botpair() {
-	local botpair=$(get_botpair_with_highest_prio)
-	local prio=$(get_prio $botpair)
+exec_correct_bot() {
+	local bot=$(get_bot_with_highest_prio)
+	local prio=$(get_prio $bot)
 	if [ ! $prio == 0 ]; then
-		exec_botpair $botpair
+		exec_bot $bot
 	fi
 }
 
-init_botcache
-create_missing_botpairs
-exec_correct_botpair
+init_bots
+exec_correct_bot
 git pull origin master
 
 # TODO if time is right => commit
